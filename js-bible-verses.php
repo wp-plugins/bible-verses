1d841101b98a28c661384b0b852821f61777102a
joelcj91 <joelcj91@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-02 20:25:56 +0000

    Added beta version
    git-svn-id: https://plugins.svn.wordpress.org/bible-verses/trunk@942443 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js-bible-verses.php
@@ -0,0 +1,96 @@
+<?php
+/*
+Plugin Name: JS Bible Verses
+Plugin URI: http://www.joelsays.com/
+Description: Shows random Bible verses as widget or using shortcode.
+Version: 1.0
+Author: Joel James
+Author URI: http://www.joelsays.com
+License: GPL2
+*/
+function js_bible_verses_stylesheet() {
+	wp_register_style( 'js-style', plugins_url('css/js-bible-verses.css', __FILE__) );
+	wp_enqueue_style( 'js-style' );
+}
+
+add_action( 'wp_enqueue_scripts', 'js_bible_verses_stylesheet' );
+
+
+function js_random_verse() {
+	$languageAdd = '';
+	$position = rand(0, 200);
+	$jsrandomVerse = get_option('jsrandomVerse_' . $position . $languageAdd);
+	$jsrandomVerse_fetch = get_option('jsrandomVerse_fetch' . $languageAdd);
+	
+	if($jsrandomVerse == "" && $jsrandomVerse_fetch < (date('U') - 3600))
+	{
+		$url = 'http://dailyverses.net/getrandomverse.ashx?language=en&position=' . $position . '&url=' . $_SERVER['HTTP_HOST'] . '&type=random1_6';
+		$result = wp_remote_get($url);
+
+		if(!is_wp_error($result)) 
+		{
+			$jsrandomVerse = str_replace(',', '&#44;', $result['body']);
+
+			update_option('jsrandomVerse_' . $position . $languageAdd, $jsrandomVerse);
+		}
+		else
+		{
+			update_option('jsrandomVerse_fetch' . $languageAdd, date('U'));
+		}
+	}
+
+	if($jsrandomVerse == "")
+	{
+		$jsrandomVerse = '<div class="dailyVerses bibleText">For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.</div><div class="dailyVerses bibleVerse">John 3:16</div>';
+	}
+	
+	$html = $jsrandomVerse;
+	
+	return $html;
+}
+
+add_shortcode('js-bible-verses', 'js_random_verse'); 
+
+
+class JSVerseWidget extends WP_Widget
+{
+  function JSVerseWidget()
+  {
+    $widget_ops = array('classname' => 'JSRandomVerse', 'description' => 'Shows good bible verses randomly on each refresh!' );
+    $this->WP_Widget('JSVerseWidget', 'JS Bible Verses', $widget_ops);
+  }
+ 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => 'Random bible verse') );
+    $title = $instance['title'];
+	
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+ 
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+ 
+    if (!empty($title))
+      echo $before_title . $title . $after_title;;
+	
+    echo js_random_verse();
+ 
+    echo $after_widget;
+  }
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("JSVerseWidget");') );
+?>
---
+++ readme.txt
@@ -0,0 +1,83 @@
+=== Bible Verses ===
+Contributors: joelcj91
+Tags: bible, bible verses, random bible verses, bible verses widget, bible widget, bible verse
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XUVWY8HUBUXY4
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Shows random Bible Verses as widget or using shortcode on each page visits.
+
+== Description ==
+
+This plugin will create a widget that will show bible verses in random order. Each time user visits it will show different verses from dailyverses.net.
+
+Apart from widget you can use shortcode [js-bible-verses]. It can be used anywhere in the page. It will also show verses.
+
+Visit <a href="http://www.joelsays.com/">Plugin Official Website</a>
+
+
+Features of Bible Verses are,
+
+ * You can use SHORTCODE to show random bible verses.
+ * You can use widgets to show bible verses in random order.
+ * Quick <a href="http://www.joelsays.com/contact-me">Direct Support</a>.
+ * Free to use with lifetime updates.
+ * More feature coming soon.. (You can suggest also)
+
+ Please contact Joel James (developer) using <a href="http://www.joelsays.com/contact-me" target="_blank">this form </a>if you have any issues.
+ Visit developer's blog - <a href="http://www.joelsays.com/contact-me" target="_blank">Joel Says</a>
+ 
+== Support ==
+Please contact author directly here http://www.joelsays.com/contact-me
+
+Current Version: 1.0 (Beta)
+
+Author: Joel James
+Author URI: http://www.joelsays.com/
+
+
+== Installation ==
+
+1. Upload `Bible Verses` plug-in zip file to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. You will see a new Widgets on your widgets manager.
+4. Drag and Drop widget to the position where you want to show bible verses.
+5. If you want to show bible verses without widgets in pages or posts, use shortcode [js-bible-verses].
+
+
+Note : Customization of appearance is coming soon. Stay tuned
+
+
+== Screenshots ==
+
+screenshots are coming soon!!
+
+== Frequently Asked Questions ==
+
+= Will this plugin show different verses always? =
+
+Really no. But it will show 200 different posts. And will not repeat quickly.
+
+= Is this plugin is free to use? =
+
+Yes. It is completely free.
+
+= Is the verses are really from Holy Bible? =
+
+Yes. For 100% sure. We will show references too.
+
+
+== Changelog ==
+
+= 1.0 =
+* Added first version without admin menu
+
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+First version.
